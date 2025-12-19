473eb012ca0fad2ed010880766d4e2a887915dc0
ahmedsami019 <easami019192@gmail.com>
2025-12-19 23:28:00 +0600

    added form and handler function

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -3,10 +3,27 @@ import React, { use } from 'react';
 const Users = ({userPromise}) => {
 
     const users = use(userPromise)
-    console.log(users);
+    // console.log(users);
+    
+    // event handler 
+    const handleAddUser = (e)=>{
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const user = {name, email}
+        console.log(user);
+    }
 
     return (
         <div>
+
+            <form onSubmit={handleAddUser}>
+                <input type="text" name='name' />
+                <br />
+                <input type="email" name='email' />
+                <br />
+                <input type="submit" value="Add user" />
+            </form>
             <div>
                 {
                     users.map(user => <p key={user.id}>{user.name} : {user.email}</p>)
