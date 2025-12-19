060cb1124df7a4f0bc4f090a92d1b1ffb9c657a5
ahmedsami019 <easami019192@gmail.com>
2025-12-19 23:13:37 +0600

    loaded the data form user server

---
+++ src/Components/Users.jsx
@@ -0,0 +1,15 @@
+import React, { use } from 'react';
+
+const Users = ({userPromise}) => {
+
+    const users = use(userPromise)
+    console.log(users);
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
